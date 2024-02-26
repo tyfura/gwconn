d7d1ef1949be72aa24c738e6d236067638f13f2c
PetaJeek <petr@jeek.cz>
2024-02-26 09:53:25 +0100

    renamed GW service

--- gw_grpc.pb.go
+++ gw_grpc.pb.go
@@ -20,126 +20,126 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	GwApi_GetNewBridges_FullMethodName = "/gwconn.GwApi/GetNewBridges"
-	GwApi_AckBridge_FullMethodName     = "/gwconn.GwApi/AckBridge"
+	GW_GetNewBridges_FullMethodName = "/gwconn.GW/GetNewBridges"
+	GW_AckBridge_FullMethodName     = "/gwconn.GW/AckBridge"
 )
 
-// GwApiClient is the client API for GwApi service.
+// GWClient is the client API for GW service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type GwApiClient interface {
+type GWClient interface {
 	GetNewBridges(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*NewBridgeList, error)
 	AckBridge(ctx context.Context, in *AcceptBridge, opts ...grpc.CallOption) (*empty.Empty, error)
 }
 
-type gwApiClient struct {
+type gWClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewGwApiClient(cc grpc.ClientConnInterface) GwApiClient {
-	return &gwApiClient{cc}
+func NewGWClient(cc grpc.ClientConnInterface) GWClient {
+	return &gWClient{cc}
 }
 
-func (c *gwApiClient) GetNewBridges(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*NewBridgeList, error) {
+func (c *gWClient) GetNewBridges(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*NewBridgeList, error) {
 	out := new(NewBridgeList)
-	err := c.cc.Invoke(ctx, GwApi_GetNewBridges_FullMethodName, in, out, opts...)
+	err := c.cc.Invoke(ctx, GW_GetNewBridges_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *gwApiClient) AckBridge(ctx context.Context, in *AcceptBridge, opts ...grpc.CallOption) (*empty.Empty, error) {
+func (c *gWClient) AckBridge(ctx context.Context, in *AcceptBridge, opts ...grpc.CallOption) (*empty.Empty, error) {
 	out := new(empty.Empty)
-	err := c.cc.Invoke(ctx, GwApi_AckBridge_FullMethodName, in, out, opts...)
+	err := c.cc.Invoke(ctx, GW_AckBridge_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// GwApiServer is the server API for GwApi service.
-// All implementations must embed UnimplementedGwApiServer
+// GWServer is the server API for GW service.
+// All implementations must embed UnimplementedGWServer
 // for forward compatibility
-type GwApiServer interface {
+type GWServer interface {
 	GetNewBridges(context.Context, *empty.Empty) (*NewBridgeList, error)
 	AckBridge(context.Context, *AcceptBridge) (*empty.Empty, error)
-	mustEmbedUnimplementedGwApiServer()
+	mustEmbedUnimplementedGWServer()
 }
 
-// UnimplementedGwApiServer must be embedded to have forward compatible implementations.
-type UnimplementedGwApiServer struct {
+// UnimplementedGWServer must be embedded to have forward compatible implementations.
+type UnimplementedGWServer struct {
 }
 
-func (UnimplementedGwApiServer) GetNewBridges(context.Context, *empty.Empty) (*NewBridgeList, error) {
+func (UnimplementedGWServer) GetNewBridges(context.Context, *empty.Empty) (*NewBridgeList, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetNewBridges not implemented")
 }
-func (UnimplementedGwApiServer) AckBridge(context.Context, *AcceptBridge) (*empty.Empty, error) {
+func (UnimplementedGWServer) AckBridge(context.Context, *AcceptBridge) (*empty.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AckBridge not implemented")
 }
-func (UnimplementedGwApiServer) mustEmbedUnimplementedGwApiServer() {}
+func (UnimplementedGWServer) mustEmbedUnimplementedGWServer() {}
 
-// UnsafeGwApiServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to GwApiServer will
+// UnsafeGWServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to GWServer will
 // result in compilation errors.
-type UnsafeGwApiServer interface {
-	mustEmbedUnimplementedGwApiServer()
+type UnsafeGWServer interface {
+	mustEmbedUnimplementedGWServer()
 }
 
-func RegisterGwApiServer(s grpc.ServiceRegistrar, srv GwApiServer) {
-	s.RegisterService(&GwApi_ServiceDesc, srv)
+func RegisterGWServer(s grpc.ServiceRegistrar, srv GWServer) {
+	s.RegisterService(&GW_ServiceDesc, srv)
 }
 
-func _GwApi_GetNewBridges_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _GW_GetNewBridges_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(empty.Empty)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GwApiServer).GetNewBridges(ctx, in)
+		return srv.(GWServer).GetNewBridges(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: GwApi_GetNewBridges_FullMethodName,
+		FullMethod: GW_GetNewBridges_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GwApiServer).GetNewBridges(ctx, req.(*empty.Empty))
+		return srv.(GWServer).GetNewBridges(ctx, req.(*empty.Empty))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _GwApi_AckBridge_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _GW_AckBridge_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(AcceptBridge)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GwApiServer).AckBridge(ctx, in)
+		return srv.(GWServer).AckBridge(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: GwApi_AckBridge_FullMethodName,
+		FullMethod: GW_AckBridge_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GwApiServer).AckBridge(ctx, req.(*AcceptBridge))
+		return srv.(GWServer).AckBridge(ctx, req.(*AcceptBridge))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// GwApi_ServiceDesc is the grpc.ServiceDesc for GwApi service.
+// GW_ServiceDesc is the grpc.ServiceDesc for GW service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var GwApi_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "gwconn.GwApi",
-	HandlerType: (*GwApiServer)(nil),
+var GW_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "gwconn.GW",
+	HandlerType: (*GWServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "GetNewBridges",
-			Handler:    _GwApi_GetNewBridges_Handler,
+			Handler:    _GW_GetNewBridges_Handler,
 		},
 		{
 			MethodName: "AckBridge",
-			Handler:    _GwApi_AckBridge_Handler,
+			Handler:    _GW_AckBridge_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
