39191f29b5d2c2100402d8fd24463f152c210e0f
PetaJeek <petr@jeek.cz>
2024-03-08 21:36:54 +0100

    BridgeTarget action and type

--- const.go
+++ const.go
@@ -3,3 +3,17 @@ package gwconn
 const (
 	GW_RpcPort = "42000"
 )
+
+// Bridge targets actions
+const (
+	BridgeTargetActAdd = iota + 1
+	BridgeTargetActDel
+	BridgeTargetActDisable
+	BridgeTargetActEnable
+)
+
+// Bridge targets type
+const (
+	BridgeTargetTypPlain = iota + 1
+	BridgeTargetTypSsl
+)
